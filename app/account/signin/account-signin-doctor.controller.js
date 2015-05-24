3d535453352430588b89ea5a3c3e4a1ac3ae9d74
sevenlol <sevenlol1007@gmail.com>
2015-05-25 01:20:31 +0800

    [master] Implement signin-doctor template and controller

--- app/account/signin/account-signin-doctor.controller.js
+++ app/account/signin/account-signin-doctor.controller.js
@@ -5,18 +5,53 @@
            .controller('accountSignInDoctorController', accountSignInDoctorController);
 
     accountSignInDoctorController.$inject = [
+        'authService',
+        '$rootScope'
     ];
 
-    function accountSignInDoctorController() {
+    function accountSignInDoctorController(authService, $rootScope) {
         var vm = this;
 
         // state variable: credentials
+        vm.credentials = {};
+
         vm.signIn = signIn;
 
         /* public functions */
 
         function signIn() {
-            // body...
+            if (!vm.credentials.username || !vm.credentials.password)
+                return;
+
+            var signInSuccessCallback = (function($rootScope) {
+                return function(data) {
+                    if (!angular.isObject(data.principal.account) ||
+                        !data.principal.account) {
+                        $rootScope.authenticated = false;
+                        $rootScope.account = null;
+                        return;
+                    }
+
+                    // Check user type
+                    if (data.principal.account.user_type !== 'DOCTOR') {
+                        $rootScope.authenticated = false;
+                        $rootScope.account = null;
+                        return;
+                    }
+
+                    $rootScope.authenticated = true;
+                    $rootScope.account = data.principal.account;
+                }
+            })($rootScope);
+
+            var signInFailCallback = (function($rootScope) {
+                return function(error) {
+                    $rootScope.authenticated = false;
+                    $rootScope.account = null;
+                }
+            })($rootScope);
+
+            authService.signIn(vm.credentials, signInSuccessCallback, signInFailCallback);
         }
     }
 
