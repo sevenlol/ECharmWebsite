d7d8d9ecf6b30397f502ef92969ac7f6c1c454c9
sevenlol <sevenlol1007@gmail.com>
2015-06-08 22:09:26 +0800

    [master] Implement doctor profile - article list

--- app/account/me/account-me-doctor-article.controller.js
+++ app/account/me/account-me-doctor-article.controller.js
@@ -5,13 +5,20 @@
            .controller('accountMeDoctorArticleController', accountMeDoctorArticleController);
 
     accountMeDoctorArticleController.$inject = [
-        'articleList'
+        'articleList',
+        'myAccount'
     ];
 
-    function accountMeDoctorArticleController(articleList) {
+    function accountMeDoctorArticleController(articleList, myAccount) {
         var vm = this;
 
         vm.articleList = articleList;
+        vm.doctorName = myAccount.user_info.name;
+
+        vm.numberOfArticle = 0;
+        if (angular.isArray(articleList) && articleList.length > 0) {
+            vm.numberOfArticle = articleList.length;
+        }
     }
 
 })();
