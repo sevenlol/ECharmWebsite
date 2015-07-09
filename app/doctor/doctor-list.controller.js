99acc15c41fbac7a808b75332b4455f8b0d9fff9
sevenlol <sevenlol1007@gmail.com>
2015-07-10 02:11:17 +0800

    [master] Doctor list template/functions

--- app/doctor/doctor-list.controller.js
+++ app/doctor/doctor-list.controller.js
@@ -9,8 +9,29 @@
     ];
 
     function doctorListController(doctorList) {
+        var NUM_OF_DOCTORS_IN_ROW = 6;
+
         var vm = this;
-        vm.doctorList = doctorList;
+        vm.doctorList = [];
+        vm.numOfDoctors = 0;
+
+        if (doctorList && angular.isArray(doctorList) && doctorList.length > 0) {
+            vm.numOfDoctors = doctorList.length;
+
+            var doctorRow = [];
+            for (var i in doctorList) {
+                doctorRow.push(doctorList[i]);
+
+                if (i % NUM_OF_DOCTORS_IN_ROW === NUM_OF_DOCTORS_IN_ROW - 1) {
+                    vm.doctorList.push(doctorRow);
+                    doctorRow = [];
+                }
+            }
+
+            if (doctorRow.length > 0) {
+                vm.doctorList.push(doctorRow);
+            }
+        }
     }
 
 })();
