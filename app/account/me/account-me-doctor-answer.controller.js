f68c81d41ce9d8d5896141ccfaec66187fd6af8f
sevenlol <sevenlol1007@gmail.com>
2015-07-09 22:12:48 +0800

    [master] Modify template and implement functions of account.me.doctor.answer

--- app/account/me/account-me-doctor-answer.controller.js
+++ app/account/me/account-me-doctor-answer.controller.js
@@ -9,8 +9,51 @@
     ];
 
     function accountMeDoctorAnswerController(answerList) {
+        var SHOW_MORE_QUESTION_STEP = 1;
+
         var vm = this;
-        vm.answerList = answerList;
+
+        vm.numOfQuestions = 0;
+        vm.questionList = answerList;
+
+        vm.pageLimit = 1;
+        vm.index = 0;
+
+        vm.displayShowMoreButton = false;
+        vm.showMoreQuestion  = showMoreQuestion;
+
+        // set number of questions
+        if (vm.questionList && angular.isArray(vm.questionList)) {
+            vm.numOfQuestions = vm.questionList.length;
+            vm.displayShowMoreButton = vm.numOfQuestions > vm.pageLimit ? true : false
+        }
+
+        if (vm.numOfQuestions > 0) {
+            // check if answered
+            for (var i in vm.questionList) {
+                if (vm.questionList[i].answer) {
+                    vm.questionList[i].isAnswered = true;
+                } else {
+                    vm.questionList[i].isAnswered = false;
+                }
+            }
+        }
+
+        /* public functions */
+
+        function showMoreQuestion() {
+            if (!vm.numOfQuestions) {
+                return;
+            }
+
+            if (vm.index + vm.pageLimit + SHOW_MORE_QUESTION_STEP >= vm.numOfQuestions) {
+                vm.pageLimit = vm.numOfQuestions;
+                vm.displayShowMoreButton = false;
+            } else {
+                vm.displayShowMoreButton = true;
+                vm.pageLimit += SHOW_MORE_QUESTION_STEP;
+            }
+        }
     }
 
 })();
