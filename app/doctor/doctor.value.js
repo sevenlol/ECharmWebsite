53bf7d12d68a603d326891cfb1b9e86868f4d650
sevenlol <sevenlol1007@gmail.com>
2015-07-11 16:16:55 +0800

    [master] Implement category navigation in doctor.list

--- app/doctor/doctor.value.js
+++ app/doctor/doctor.value.js
@@ -3,6 +3,7 @@
 
     angular.module('app.doctor')
            .value('doctorCategoryList', [
+           	    { name : 'all', text : '所有醫師' },
            		{ name : 'Category_1', text : '類別1' },
            		{ name : 'Category_2', text : '類別2' }
            	]);
