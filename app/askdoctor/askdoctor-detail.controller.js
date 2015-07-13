e2586bed9080a83fea826fe78866ec73d08766fd
sevenlol <sevenlol1007@gmail.com>
2015-07-14 02:02:09 +0800

    [master] Modify askdoctor detail template and implement controller functions

--- app/askdoctor/askdoctor-detail.controller.js
+++ app/askdoctor/askdoctor-detail.controller.js
@@ -6,28 +6,65 @@
 
     askdoctorDetailController.$inject = [
         'question',
+        'answer',
         'doctor',
         'user',
         'commentList',
         'ratingList',
-        'avgRating'
+        'avgRating',
+        'askdoctorCategoryList'
     ];
 
-    function askdoctorDetailController(question, doctor, user, commentList, ratingList, avgRating) {
+    function askdoctorDetailController(question, answer, doctor, user, commentList, ratingList, avgRating, askdoctorCategoryList) {
         var vm = this;
 
         vm.question = question;
+        vm.answer = answer;
         vm.doctor = doctor;
         vm.user = user;
         vm.commentList = commentList;
         vm.avgRating = avgRating;
+        vm.categoryName = getCategoryName(askdoctorCategoryList, question);
 
         vm.answerThisQuestion = answerThisQuestion;
+        vm.commentThisQuestion = commentThisQuestion;
+        vm.rateThisQuestion = rateThisQuestion;
 
         /* public functions */
 
         function answerThisQuestion() {
             // body...
+        }
+
+        function commentThisQuestion() {
+
+        }
+
+        function rateThisQuestion() {
+            // body...
+        }
+
+        /* private functions */
+        function getCategoryName(categoryList, question) {
+            if (!categoryList || !angular.isArray(categoryList) || categoryList === 0) {
+                return '';
+            }
+
+            if (!question || !angular.isObject(question)) {
+                return '';
+            }
+
+            for (var i in categoryList) {
+                if (!categoryList[i]) {
+                    continue;
+                }
+
+                if (categoryList[i].name === question.category) {
+                    return categoryList[i].text;
+                }
+            }
+
+            return '';
         }
     }
 
