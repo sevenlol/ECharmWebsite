b254e0e65b2d9227bb6342992c67286138cfede3
sevenlol <sevenlol1007@gmail.com>
2015-07-15 00:51:43 +0800

    [master] Implement submit answer function and fix typo

--- app/askdoctor/askdoctor-detail.controller.js
+++ app/askdoctor/askdoctor-detail.controller.js
@@ -5,6 +5,7 @@
            .controller('askdoctorDetailController', askdoctorDetailController);
 
     askdoctorDetailController.$inject = [
+        'myAccount',
         'question',
         'answer',
         'doctor',
@@ -13,10 +14,11 @@
         'commentUserList',
         'ratingList',
         'avgRating',
-        'askdoctorCategoryList'
+        'askdoctorCategoryList',
+        'askdoctorAnswerService'
     ];
 
-    function askdoctorDetailController(question, answer, doctor, user, commentList, commentUserList, ratingList, avgRating, askdoctorCategoryList) {
+    function askdoctorDetailController(myAccount, question, answer, doctor, user, commentList, commentUserList, ratingList, avgRating, askdoctorCategoryList, askdoctorAnswerService) {
         var vm = this;
 
         vm.question = question;
@@ -29,6 +31,15 @@
         vm.categoryName = getCategoryName(askdoctorCategoryList, question);
         vm.categoryList = askdoctorCategoryList;
 
+        // answer form
+        vm.answerMin = 20;
+        vm.answerSubmitted = false;
+        vm.answerStatusMessage = {
+            isShown : false,
+            type    : 'error',
+            message : ''
+        };
+
         // comment form
         vm.min = 10;
         vm.commentSubmitted = false;
@@ -40,7 +51,56 @@
         /* public functions */
 
         function answerThisQuestion() {
-            // body...
+            vm.answerSubmitted = true;
+            vm.answerStatusMessage.isShown = false;
+
+            // invalid answer text
+            if (vm.answerForm.answerText.$invalid) {
+                return;
+            }
+
+            if (!askdoctorAnswerService || !myAccount ||
+                myAccount.user_type !== 'DOCTOR' || !myAccount.account_id) {
+                submitAnswerFailed('發表解答時發生錯誤，請稍後重新嘗試');
+            }
+
+            // TODO change this
+            var answerBody = {
+                answerer_id : myAccount.account_id,
+                created_at : new Date().toString(),
+                updated_at : new Date().toString(),
+                answer_text : vm.answerText
+            };
+
+            // callbacks
+            var submitAnswerSuccessCallback = function(answer) {
+                // TODO add handling code
+                // create failed
+                if (!answer) {
+                    submitAnswerFailed('發表解答時發生錯誤，請稍後重新嘗試');
+                    return;
+                }
+
+                submitAnswerSucceeded('解答發表成功');
+                // created succeeded
+            }
+            var submitAnswerFailCallback = function(error) {
+                if (error && error.message) {
+                    submitAnswerFailed(error.message);
+                    return;
+                }
+
+                submitAnswerFailed('發表解答時發生錯誤，請稍後重新嘗試');
+            };
+
+            try {
+                askdoctorAnswerService
+                    .createAnswer(vm.question.category, vm.question.question_id, answerBody)
+                    .then(submitAnswerSuccessCallback)
+                    .catch(submitAnswerFailCallback);
+            } catch (e) {
+                submitAnswerFailed('發表解答時發生錯誤，請稍後重新嘗試');
+            }
         }
 
         function commentThisQuestion() {
@@ -52,6 +112,21 @@
         }
 
         /* private functions */
+
+        function submitAnswerFailed(msg) {
+            vm.answerSubmitted = false;
+            vm.answerStatusMessage.isShown = true;
+            vm.answerStatusMessage.type = 'error';
+            vm.answerStatusMessage.message = msg;
+        }
+
+        function submitAnswerSucceeded(msg) {
+            vm.answerSubmitted = false;
+            vm.answerStatusMessage.isShown = true;
+            vm.answerStatusMessage.type = 'success';
+            vm.answerStatusMessage.message = msg;
+        }
+
         function getCategoryName(categoryList, question) {
             if (!categoryList || !angular.isArray(categoryList) || categoryList === 0) {
                 return '';
