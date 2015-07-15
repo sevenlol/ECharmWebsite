2c20f6cbd3e8439fdbbbc5f3f877555d98290083
Coralleaf <danielchenchunyu@gmail.com>
2015-07-15 20:37:39 +0800

    blog put into grid implemented

--- app/blog/blog-list.controller.js
+++ app/blog/blog-list.controller.js
@@ -10,17 +10,41 @@
     ];
 
     function blogListController(articleList, doctorList) {
+        var NUM_OF_ARTICLES_IN_ROW = 5;
+
         var vm = this;
 
-        var articleGrid = null; // This object is used to store the article objects to be displayed in ng-repeat
+        vm.articleGrid = []; // This object is used to store the article objects to be displayed in ng-repeat
 
         // TODO fix this
         vm.articleList = articleList;
+        //vm.category = $stateParams ? $stateParams.category : 'all';
+        genArticleGrid(vm, articleList);
+        console.log("Total: " + vm.numOfArticles + ", Row: " + vm.articleGrid[0]);
         // To do generate grid object here
-        // articleGrid = [[article_1, article_2, article_3],[],[],...]
-        function genArticleGrid() {
-            // TODO
+        // articleGrid = [[article_1, article_2, article_3...],[],[],...]
+        
+        function genArticleGrid(vmObj, list) {
+
+            var resultArr = [];
+
+            if (list && angular.isArray(list) && list.length > 0) {
+                vmObj.numOfArticles = list.length;
+
+                var articleRow = [];
+                for (var i in list) {
+                    articleRow.push(list[i]);
+
+                    if (i % NUM_OF_ARTICLES_IN_ROW === NUM_OF_ARTICLES_IN_ROW - 1) {
+                        vm.articleGrid.push(articleRow);
+                        articleRow = [];
+                    }
+                }
+
+                if (articleRow.length > 0) {
+                    vm.articleGrid.push(articleRow);
+                }
+            }
         }
     }
-
 })();
