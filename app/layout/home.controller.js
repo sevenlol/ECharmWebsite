06c50386ec0c132c15ec5e90d3c645b0930f9fbc
sevenlol <sevenlol1007@gmail.com>
2015-07-21 02:01:03 +0800

    [master] Add animation when hovering over main-kv's heading

--- app/layout/home.controller.js
+++ app/layout/home.controller.js
@@ -10,6 +10,22 @@
     ];
 
     function homeController(authService, $rootScope) {
+    	var vm = this;
+
+    	vm.showInstruction = false;
+        // FIXME remove this if the instruction text is not needed
+    	/*vm.enterDoctorListLink = enterDoctorListLink;
+    	vm.leaveDoctorListLink = leaveDoctorListLink;*/
+
+
+    	/* public functions */
+    	/*function enterDoctorListLink() {
+    		vm.showInstruction = true;
+    	}
+
+    	function leaveDoctorListLink() {
+    		vm.showInstruction = false;
+    	}*/
     }
 
 })();
