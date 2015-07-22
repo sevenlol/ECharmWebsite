fe967f184eb545838361d62101023035beab5d4b
Coralleaf <danielchenchunyu@gmail.com>
2015-07-22 20:38:04 +0800

    Change comment style in blog

--- app/blog/blog-detail-comment.controller.js
+++ app/blog/blog-detail-comment.controller.js
@@ -18,13 +18,25 @@
         var COMMNET_MIN_LENGTH = 10;
         var DATE_FORMAT = 'yyyy-MM-ddTHH:mmZ';
 
+        // comment form
+        vm.min = 10;
+        vm.commentSubmitted = false;
+        vm.commentStatusMessage = {
+            isShown : false,
+            type    : 'error',
+            message : ''
+        };
+
         vm.commentThisArticle = commentThisArticle;
         vm.category = $stateParams ? $stateParams.category : 'all';
         vm.articleId = $stateParams.articleId;
         vm.commentMessage = "";
-        vm.commentContent = "";
+        vm.commentText = "";
         vm.commentSucc = false;
+        vm.myAccount = myAccount;
+        vm.commentThisArticle = commentThisArticle;
         /* public functions */
+        /*
         function commentThisArticle() {
             // body...
             // 1. Check if the length of the comment is greater than the specified length
@@ -57,6 +69,84 @@
                     vm.commentSucc = false;
                 }
                 
+            }
+        }
+        */
+        function commentThisArticle() {
+            vm.commentSubmitted = true;
+            vm.commentStatusMessage.isShown = false;
+
+            // invalid comment text
+            if (vm.commentForm.commentText.$invalid) {
+                return;
+            }
+
+            if (!blogCommentService || !myAccount || !myAccount.account_id) {
+                submitCommentFailed('發表評論時發生錯誤，請稍後重新嘗試');
+                return;
+            }
+
+            var commentBody = {
+                commenter_id : myAccount.account_id,
+                created_at : $filter('date')(new Date(), DATE_FORMAT),
+                updated_at : $filter('date')(new Date(), DATE_FORMAT),
+                comment_text : vm.commentText,
+                author_response_text: "asd",
+                responded_at: $filter('date')(new Date(), DATE_FORMAT)
+            };
+
+            // callbacks
+            var submitCommentSuccessCallback = function(comment) {
+                // TODO add handling code
+                // create failed
+                if (!comment) {
+                    submitCommentFailed('發表評論時發生錯誤，請稍後重新嘗試');
+                    return;
+                }
+
+                submitCommentSucceeded('評論發表成功', comment);
+                // created succeeded
+            }
+            var submitCommentFailCallback = function(error) {
+                if (error && error.message) {
+                    submitCommentFailed(error.message);
+                    return;
+                }
+
+                submitCommentFailed('發表評論時發生錯誤，請稍後重新嘗試');
+            };
+
+            try {
+                blogCommentService
+                    .createComment(vm.category, vm.articleId, commentBody)
+                    .then(submitCommentSuccessCallback)
+                    .catch(submitCommentFailCallback);
+            } catch (e) {
+                submitCommentFailed('發表評論時發生錯誤，請稍後重新嘗試');
+            }
+        }
+
+        function submitCommentFailed(msg) {
+            vm.commentSubmitted = false;
+            vm.commentStatusMessage.isShown = true;
+            vm.commentStatusMessage.type = 'error';
+            vm.commentStatusMessage.message = msg;
+        }
+
+        function submitCommentSucceeded(msg, comment) {
+            vm.commentSubmitted = false;
+            vm.commentStatusMessage.isShown = true;
+            vm.commentStatusMessage.type = 'success';
+            vm.commentStatusMessage.message = msg;
+            vm.commentText = '';
+
+            // add myAccount to comment
+            comment.user = vm.myAccount;
+
+            if (!vm.commentList || !angular.isArray(vm.commentList) || vm.commentList.length === 0) {
+                vm.commentList = [ comment ];
+            } else {
+                vm.commentList.push(comment);
             }
         }
     }
