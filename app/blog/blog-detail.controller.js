a4ba35127c5c772b18300a5ce90d032f4fb1f97d
Chun-Yu Chen <danielchenchunyu@gmail.com>
2015-07-23 20:00:25 +0800

    Add rating to blog-detail html

--- app/blog/blog-detail.controller.js
+++ app/blog/blog-detail.controller.js
@@ -19,16 +19,127 @@
         vm.article = article;
         vm.author = author;
         vm.commentList = commentList;
+        vm.ratingList = ratingList;
         vm.avgRating = avgRating;
 
         vm.rateThisArticle = rateThisArticle;
+
+        vm.hoverOverRating = hoverOverRating;
+        vm.collapseMyRatingBar = collapseMyRatingBar;
 
         console.log(JSON.stringify(author, null, 2));
 
         /* public functions */
 
+        function hoverOverRating(value) {
+            if (vm.ratingList && vm.ratingList.length) {
+                vm.overRatingValue = '平均 ' + ((vm.avgRating % 1 === 0) ? vm.avgRating : $filter('number')(vm.avgRating, AVG_RATING_PRECISION));
+                vm.overRatingValue += ' 分 總共' + vm.ratingList.length + '次評分紀錄';
+            } else {
+                vm.overRatingValue = '目前尚無評分紀錄';
+            }
+        }
+
         function rateThisArticle() {
-            // body...
+            if (vm.iAlreadyRated) {
+                return;
+            }
+
+            if (vm.myRating < 0 || vm.myRating > 5) {
+                return;
+            }
+
+            if (!blogRatingService || !myAccount || !myAccount.account_id) {
+                submitRatingFailed('發表評分時發生錯誤，請稍後重新嘗試');
+                return;
+            }
+
+            var ratingBody = {
+                rater_id : myAccount.account_id,
+                created_at : $filter('date')(new Date(), DATE_FORMAT),
+                updated_at : $filter('date')(new Date(), DATE_FORMAT),
+                rating_value : vm.myRating
+            };
+
+            // callbacks
+            var submitRatingSuccessCallback = function(rating) {
+                // TODO add handling code
+                // create failed
+                if (!rating) {
+                    submitRatingFailed('發表評分時發生錯誤，請稍後重新嘗試');
+                    return;
+                }
+
+                submitRatingSucceeded('評分發表成功', rating);
+                // created succeeded
+            };
+            var submitRatingFailCallback = function(error) {
+                if (error && error.message) {
+                    submitRatingFailed(error.message);
+                    return;
+                }
+
+                submitRatingFailed('發表評分時發生錯誤，請稍後重新嘗試');
+            };
+
+            try {
+                blogRatingService
+                    .createRating(vm.question.category, vm.question.question_id, ratingBody)
+                    .then(submitRatingSuccessCallback)
+                    .catch(submitRatingFailCallback);
+            } catch (e) {
+                submitAnswerFailed('發表評分時發生錯誤，請稍後重新嘗試');
+            }
+        }
+
+        function submitRatingFailed(msg) {
+            vm.iAlreadyRated = false;
+            vm.ratingStatusMessage.isShown = true;
+            vm.ratingStatusMessage.type = 'error';
+            vm.ratingStatusMessage.message = msg;
+        }
+
+        function submitRatingSucceeded(msg, rating) {
+            vm.ratingStatusMessage.isShown = true;
+            vm.ratingStatusMessage.type = 'success';
+            vm.ratingStatusMessage.message = msg;
+
+            if (!vm.iAlreadyRated && rating && angular.isNumber(rating.rating_value)) {
+                vm.iAlreadyRated  = true;
+                vm.myRating = rating.rating_value;
+                if (!vm.ratingList || !angular.isArray(vm.ratingList) || vm.ratingList.length === 0) {
+                    vm.ratingList = [ vm.myRating ];
+                    vm.avgRating = vm.myRating;
+                } else {
+                    vm.ratingList.push(vm.myRating);
+                    vm.avgRating = (vm.myRating + vm.avgRating * (vm.ratingList.length - 1)) / vm.ratingList.length;
+                }
+            }
+        }
+
+        function findMyRating(ratingList) {
+            if (!ratingList || !angular.isArray(ratingList) ||
+                ratingList.length === 0 || !myAccount || !myAccount.account_id) {
+                return false;
+            }
+
+            for (var i in ratingList) {
+                if (!ratingList[i]) {
+                    continue;
+                }
+
+                if (ratingList[i].rater_id === myAccount.account_id) {
+                    vm.iAlreadyRated = true;
+                    vm.myRating = ratingList[i].rating_value;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        function collapseMyRatingBar() {
+            vm.isMyRatingBarCollapsed = !vm.isMyRatingBarCollapsed;
         }
     }
 
