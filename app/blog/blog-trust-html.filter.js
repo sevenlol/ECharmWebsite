f8b554f5acce1e293161c15a6f791de99739f79b
ian_lin <ian_lin@gemteks.com>
2015-07-28 15:51:11 +0800

    [master] Add type check for trust html filter

--- app/blog/blog-trust-html.filter.js
+++ app/blog/blog-trust-html.filter.js
@@ -4,6 +4,10 @@
     angular.module('app.blog')
         .filter('trustHtmlFilter', ['$sce', function($sce){
             return function(text) {
+                if (!text || !angular.isString(text)) {
+                    return text;
+                }
+
                 return $sce.trustAsHtml(text);
             };
         }]);
