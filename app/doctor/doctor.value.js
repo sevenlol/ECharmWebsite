535edd1d9de4076d7043ff46fa2b43ecdfff1358
ian_lin <ian_lin@gemteks.com>
2015-09-11 10:42:15 +0800

    [master] Rearrange the order of doctor category list

--- app/doctor/doctor.value.js
+++ app/doctor/doctor.value.js
@@ -3,14 +3,14 @@
 
     angular.module('app.doctor')
            .value('doctorCategoryList', [
-           	    { name : 'all', text : '所有醫師' },
-           		{ name : 'PGY', text : '不分科' },
-	            { name : 'plasticsurgeon', text : '整形外科' },
-	            { name : 'cosmeticsurgeon', text : '醫學美容' },
-	            { name : 'dermatologist', text : '皮膚科' },
-	            { name : 'pediatrician', text : '小兒科' },
-	            { name : 'OBSGYN', text : '婦產科' },
-	            { name : 'others', text : '其他醫師' }
-           	]);
+                { name : 'all', text : '所有醫師' },
+                { name : 'cosmeticsurgeon', text : '醫學美容' },
+                { name : 'plasticsurgeon', text : '整形外科' },
+                { name : 'OBSGYN', text : '婦產科' },
+                { name : 'dermatologist', text : '皮膚科' },
+                { name : 'pediatrician', text : '小兒科' },
+                { name : 'PGY', text : '不分科' },
+                { name : 'others', text : '其他醫師' }
+            ]);
 
 })();
