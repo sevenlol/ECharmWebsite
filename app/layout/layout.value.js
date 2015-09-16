e9804ea0960c63cbcc722004ce930c4394943a29
ian_lin <ian_lin@gemteks.com>
2015-09-16 10:52:58 +0800

    [master] Update doctor covers in home.html

--- app/layout/layout.value.js
+++ app/layout/layout.value.js
@@ -4,9 +4,9 @@
     angular
         .module('app.layout')
         .value('frontPageDoctorSlideList', [
-            { doctor_title: '國泰醫院婦產科 權威', photo_url : 'images/index-doctor-01.jpg', sign_url : 'images/sign.png' },
-            { doctor_title: '台大醫院骨科 主任', photo_url : 'images/index-doctor-01.jpg', sign_url : 'images/sign.png' },
-            { doctor_title: '中山醫院婦科 主治醫生', photo_url : 'images/index-doctor-01.jpg', sign_url : 'images/sign.png' }
+            { doctor_title: '癌症權威 腫瘤科主任', photo_url : 'images/aboutus/0.png', sign_url : 'images/sign.png' },
+            { doctor_title: '癌症權威 腫瘤科主任', photo_url : 'images/aboutus/0.png', sign_url : 'images/sign.png' },
+            { doctor_title: '癌症權威 腫瘤科主任', photo_url : 'images/aboutus/0.png', sign_url : 'images/sign.png' }
         ]);
 
 })();
