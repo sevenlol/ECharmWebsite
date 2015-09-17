cafebc484251e67e015c0f9ebdec7d35cddd8c15
sevenlol <sevenlol1007@gmail.com>
2015-09-17 22:10:38 +0800

    [master] Add signature photo in home.html

--- app/layout/layout.value.js
+++ app/layout/layout.value.js
@@ -4,9 +4,9 @@
     angular
         .module('app.layout')
         .value('frontPageDoctorSlideList', [
-            { doctor_title: '癌症權威 腫瘤科主任', photo_url : 'images/doctor_cover/0.png', sign_url : 'images/sign.png' },
-            { doctor_title: '癌症權威 腫瘤科主任', photo_url : 'images/doctor_cover/0.png', sign_url : 'images/sign.png' },
-            { doctor_title: '癌症權威 腫瘤科主任', photo_url : 'images/doctor_cover/0.png', sign_url : 'images/sign.png' }
+            { doctor_title: '癌症權威 腫瘤科主任', photo_url : 'images/doctor_cover/0.png', sign_url : 'images/doctor_cover/signature0.png' },
+            { doctor_title: '癌症權威 腫瘤科主任', photo_url : 'images/doctor_cover/0.png', sign_url : 'images/doctor_cover/signature0.png' },
+            { doctor_title: '癌症權威 腫瘤科主任', photo_url : 'images/doctor_cover/0.png', sign_url : 'images/doctor_cover/signature0.png' }
         ]);
 
 })();
