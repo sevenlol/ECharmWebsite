25146d4febead1f773719a3f4a2373a1050a2df1
sevenlol <sevenlol1007@gmail.com>
2015-10-22 22:22:53 +0800

    [master] Add password reset page (UI only)

---
+++ app/account/account-password-reset.controller.js
@@ -0,0 +1,16 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('app.account')
+		.controller('accountPasswordResetController', accountPasswordResetController);
+
+	accountPasswordResetController.$inject = [
+	];
+
+	function accountPasswordResetController() {
+
+		var vm = this;
+	}
+
+})();
