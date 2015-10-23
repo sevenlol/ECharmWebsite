e85ebdfd4229c7105b5c50b3993eaf3695f92420
sevenlol <sevenlol1007@gmail.com>
2015-10-23 21:42:36 +0800

    [master] Implement password change function for user (not notification yet)

--- app/account/me/account-me-user-setting.controller.js
+++ app/account/me/account-me-user-setting.controller.js
@@ -1,15 +1,85 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	angular
-		.module('app.account')
-		.controller('accountMeUserSettingController', accountMeUserSettingController);
+    angular
+        .module('app.account')
+        .controller('accountMeUserSettingController', accountMeUserSettingController);
 
-	accountMeUserSettingController.$inject = [
-	];
+    accountMeUserSettingController.$inject = [
+        '$http',
+        '$rootScope',
+        'valueService'
+    ];
 
-	function accountMeUserSettingController() {
-		// body...
-	}
+    function accountMeUserSettingController(
+        $http,
+        $rootScope,
+        valueService) {
+
+        var SERVER_URL = valueService.SERVER_URL.AUTH;
+
+        var vm = this;
+
+        vm.changePassword = changePassword;
+
+        /* public functions */
+
+        function changePassword() {
+
+            if (!$rootScope.authenticated || !$rootScope.account ||
+                !$rootScope.account.username) {
+                console.log('Not logged in!');
+                return;
+            }
+
+            if (!vm.oldPassword || !vm.newPassword || !vm.newPasswordConfirm) {
+                console.log('Missing required fields!');
+                return;
+            }
+            if (vm.newPassword !== vm.newPasswordConfirm) {
+                console.log('New password wrong!');
+                return;
+            }
+
+            var successCallback = (function() {
+                return function(res) {
+                    if (res && res.status === 200) {
+                        console.log('Password Change Succeeded!');
+                        console.log(JSON.stringify(res.data));
+                    } else {
+                        console.log('Password Change Failed!');
+                        console.log('Status: ' + res.status);
+                    }
+                };
+            })();
+
+            var failCallback = function(error) {
+                console.log('Password Change Failed!');
+                console.log('Status: ' + error.status);
+            };
+
+            try {
+                changeMyPassword(SERVER_URL, $rootScope.account.username, vm.oldPassword, vm.newPassword)
+                    .then(successCallback)
+                    .catch(failCallback);
+            } catch(e) {
+                console.log('Password Change Failed!');
+                console.log('Something is wrong!');
+            }
+        }
+
+        /* private functions */
+
+        // FIXME change this to shared service
+        function changeMyPassword(baseUrl, username, oldPassword, newPassword) {
+            var url = baseUrl + '/me/password';
+
+            var headers = {
+                authorization : 'Basic ' + btoa(username + ':' + oldPassword)
+            };
+
+            return $http.put(url, { password : newPassword }, { headers : headers });
+        }
+    }
 
 })();
