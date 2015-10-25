b10c6668cefab7f203d6cf41f984ec482eb42133
sevenlol <sevenlol1007@gmail.com>
2015-10-25 19:51:54 +0800

    [master] Add password change message in account.user

--- app/account/me/account-me-user-setting.controller.js
+++ app/account/me/account-me-user-setting.controller.js
@@ -33,34 +33,40 @@
 
             if (!$rootScope.authenticated || !$rootScope.account ||
                 !$rootScope.account.username) {
-                console.log('Not logged in!');
+                setPasswordChangeMsg('請先登入已進行此動作!', false, true);
                 return;
             }
 
             if (!vm.oldPassword || !vm.newPassword || !vm.newPasswordConfirm) {
-                console.log('Missing required fields!');
+                setPasswordChangeMsg('請填寫所有需要的欄位!', false, true);
                 return;
             }
             if (vm.newPassword !== vm.newPasswordConfirm) {
-                console.log('New password wrong!');
+                setPasswordChangeMsg('新密碼和確認新密碼欄位不同!', false, true);
                 return;
             }
 
             var successCallback = (function() {
                 return function(res) {
                     if (res && res.status === 200) {
-                        console.log('Password Change Succeeded!');
-                        console.log(JSON.stringify(res.data));
+                        setPasswordChangeMsg('密碼變更成功!', true, true);
                     } else {
-                        console.log('Password Change Failed!');
-                        console.log('Status: ' + res.status);
+                        setPasswordChangeMsg('密碼變更失敗，請稍後重新嘗試!', false, true);
                     }
                 };
             })();
 
             var failCallback = function(error) {
-                console.log('Password Change Failed!');
-                console.log('Status: ' + error.status);
+                if (!error) {
+                    setPasswordChangeMsg('密碼變更失敗，請稍後重新嘗試!', false, true);
+                    return;
+                }
+
+                if (error.status === 401) {
+                    setPasswordChangeMsg('舊密碼輸入錯誤!', false, true);
+                    return;
+                }
+                setPasswordChangeMsg('密碼變更失敗，請稍後重新嘗試!', false, true);
             };
 
             try {
@@ -68,12 +74,21 @@
                     .then(successCallback)
                     .catch(failCallback);
             } catch(e) {
-                console.log('Password Change Failed!');
-                console.log('Something is wrong!');
+                setPasswordChangeMsg('密碼變更失敗，請稍後重新嘗試!', false, true);
             }
         }
 
         /* private functions */
+
+        function setPasswordChangeMsg(msg, succeeded, showMsg) {
+            var msgObj = {
+                show : showMsg,
+                type : succeeded ? 'success' : 'danger',
+                message : msg
+            };
+
+            vm.passwordChangeMsg = msgObj;
+        }
 
         // FIXME change this to shared service
         function changeMyPassword(baseUrl, username, oldPassword, newPassword) {
